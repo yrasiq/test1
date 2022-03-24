d207abf23e08532483541a3ac80a4b69c7f55997
yrasiq <yrasiq@gmail.com>
2022-03-24 17:45:22 +0300

    Update views.py

--- cities/mainapp/views.py
+++ cities/mainapp/views.py
@@ -12,7 +12,7 @@ class HomePage(ListView):
     def get_context_data(self, **kwargs):
         self.extra_context.update({
             'biggest_cities': City.objects.annotate(
-                citizens_count=Count('citizen')
+                citizens_count=Count('citizens')
             ).order_by('-citizens_count')[:5]
         })
         return super().get_context_data(**kwargs)
